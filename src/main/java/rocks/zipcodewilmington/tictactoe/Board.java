1427e8b741354a5e5cf65a996d8428e445531fdd
Corinne <CorinneKogut@gmail.com>
2023-11-04 21:44:55 -0400

    adding some pseudocode

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -4,7 +4,10 @@ package rocks.zipcodewilmington.tictactoe;
  * @author leon on 6/22/18.
  */
 public class Board {
+
+    Character[][] board;
     public Board(Character[][] matrix) {
+        this.board = matrix;
     }
 
     public Boolean isInFavorOfX() {
@@ -20,7 +23,31 @@ public class Board {
     }
 
     public String getWinner() {
+        //if favorOfX == favorOfY (
+        //it's a tie
+
+        //if it's not a tie, whichever favorOf is true is the winner
+
         return null;
     }
 
+    public Boolean checkRow(Board board, char xORy){
+
+        for (int row=0; row<3; row++){
+            for (int col=0; col<3; col++){
+
+            }
+        }
+        return null;
+    }
+
+    public Boolean checkCol(Board board, char xORy){
+
+        return null;
+    }
+
+    public Boolean checkDiag(Board board, char xORy){
+
+        return null;
+    }
 }
